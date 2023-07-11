f26c2928e2e97b390e73a55915a77484507c3c4d
Revogabe <revogabe.dev@gmail.com>
2023-07-11 02:22:44 -0300

    remove module

--- src/components/office/FormNewClientOffice.tsx
+++ src/components/office/FormNewClientOffice.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { z } from 'zod'
-import { Controller, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { parseCookies } from 'nookies'
 import { Modal } from '@/components/ui/Modal'
